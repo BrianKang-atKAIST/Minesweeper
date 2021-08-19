b97b67dea4a2afbf7491038c0b171e15e1507a85
BrianKang <kbm03@kaist.ac.kr>
2021-08-19 20:02:39 +0900

    huge surgery.... it was very hard. every main important properties changed from list into dictionary, matching //tuple(x, y) : something//.

--- mine_sweeper_GUI.py
+++ mine_sweeper_GUI.py
@@ -1,6 +1,7 @@
 from mine_sweeper_embedded import *
 from tkinter import *
 from functools import partial
+import time
 
 # 레벨 선택
 # level = input('난이도를 선택하세요: ')
@@ -18,8 +19,11 @@ TopFrame.pack(side='top', fill='x')
 MineFrame = Frame(root)
 MineFrame.pack(side='top')
 
+start_time = time.time()
 Minesweeper = mineland(MineFrame, level, 30)
+end_time = time.time()
 print(Minesweeper.minemap)
+print(end_time - start_time)
 
 # 다시하기 버튼은 mineland 클래스 안에 넣어둔다.
 Minesweeper.restart_btn = Button(TopFrame, image=Minesweeper.photo_dict['neutral'], command=Minesweeper.again)
--- mine_sweeper_embedded.py
+++ mine_sweeper_embedded.py
@@ -1,6 +1,7 @@
 # 모듈 임포트
 from random import shuffle
 from functools import partial
+import random
 from tkinter import *
 import time
 
@@ -8,6 +9,7 @@ import time
 class mineland:
   def __init__(self, root, level, blockpixel):
     '''mineland 클래스 객체 init'''
+
     # 이미지 다운로드
     self.images_list = [f'{i}' for i in range(0, 9)] + [f'{i}' for i in range(0, 9)] + ['block', 'mine', 'flag', 'unknown', 'neutral', 'dead', 'win']
     self.photo_dict = {image : PhotoImage(file=f'C:\\Users\\82102\\Desktop\\PythonWorkspace\\mine_sweeper\\images{blockpixel}px\\{image}.png') for image in self.images_list}
@@ -16,8 +18,9 @@ class mineland:
     self.level = level
     self.blockpixel = blockpixel
     self.level_dict = level_dict_dict[level]
-    self.minemap, self.mineset = self.init_mines()
-    self.buttonmap = self.init_buttons()
+    # 아래의 식에서 self.minemap, self.mineset, self.buttonmap_dict이 정의됨
+    self.lay_mine()
+    self.init_buttons()
     self.openedset = set()
     # 라벨 마우스 양쪽 버튼으로 클릭 시 나오는 이펙트 구현을 위한 상수들
     self.lefton = False # 왼쪽 버튼을 눌렀나?
@@ -42,9 +45,7 @@ class mineland:
       3) 만약 칸이 0개의 지뢰와 맞닿아 있다면 주변 모든 칸 클릭
     '''
     # print('left', coord)
-    x = coord[0]
-    y = coord[1]
-    if self.minemap[y][x] % 10 == 9:
+    if self.minemap[coord] % 10 == 9:
       self.dead()
     else:
       self.openblock(coord)
@@ -57,21 +58,19 @@ class mineland:
     10의 자리 수가 깃발, 물음표, 아무것도 없음 이 셋을 결정
     '''
     # print('right', coord)
-    x = coord[0]
-    y = coord[1]
-    self.minemap[y][x] += 10
-    if (self.minemap[y][x]//10) % 3 == 0: # 맵의 앞자리수가 0, 3, 6이면 아무것도 표시 안함
-      self.buttonmap[y][x]['image'] = self.photo_dict['block']
-      self.buttonmap[y][x]['state'] = 'active'      
+    self.minemap[coord] += 10
+    if (self.minemap[coord]//10) % 3 == 0: # 맵의 앞자리수가 0, 3, 6이면 아무것도 표시 안함
+      self.buttonmap_dict[coord]['image'] = self.photo_dict['block']
+      self.buttonmap_dict[coord]['state'] = 'active'      
     # 맵의 앞자리수가 1, 4, 7이면 깃발 표시, self.detected_mineset에 그 칸 넣음
-    elif (self.minemap[y][x]//10) % 3 == 1:
-      self.buttonmap[y][x]['image'] = self.photo_dict['flag']
-      self.buttonmap[y][x]['state'] = 'disabled'
+    elif (self.minemap[coord]//10) % 3 == 1:
+      self.buttonmap_dict[coord]['image'] = self.photo_dict['flag']
+      self.buttonmap_dict[coord]['state'] = 'disabled'
       if coord in self.mineset:
         self.detected_mineset.add(coord)
     # 맵의 앞자리수가 2, 5, 8이면 물음표 표시, self.detected_mineset에서 그 칸 제거
     else: 
-      self.buttonmap[y][x]['image'] = self.photo_dict['unknown']
+      self.buttonmap_dict[coord]['image'] = self.photo_dict['unknown']
       if coord in self.mineset:
         self.detected_mineset.remove(coord)
     # print(self.detected_mineset)
@@ -82,48 +81,41 @@ class mineland:
       if self.allopened: # allopened도 True라면? 끝
         self.end()
 
-  def init_mines(self):
-    '''minemap에 지뢰를 놓는다.'''
-    temp_minemap = [[0 for i in range(self.level_dict['col'])] for i in range(self.level_dict['row'])]
-    return lay_mine(temp_minemap, self.level_dict['mines'])
-
   def init_buttons(self):
     '''buttonmap을 생성하는 함수'''
-    temp_buttonmap = [[0 for i in range(self.level_dict['col'])] for i in range(self.level_dict['row'])]
-    for y, row in enumerate(temp_buttonmap):
-      for x, block in enumerate(row):
-        coord = (x, y)
-        temp_buttonmap[y][x] = Button(self.root, image=self.photo_dict['block'], command=partial(self.left_minecmd, coord))
-        temp_buttonmap[y][x].grid(row=y, column=x)
-        temp_buttonmap[y][x].bind('<Button-3>', partial(self.right_minecmd, coord))
-    return temp_buttonmap
+    temp_buttonmap_dict = {(i, j): 0 for i in range(self.level_dict['col']) for j in range(self.level_dict['row'])}
+    for coord in temp_buttonmap_dict.keys():
+      x = coord[0]
+      y = coord[1]
+      temp_buttonmap_dict[coord] = Button(self.root, image=self.photo_dict['block'], command=partial(self.left_minecmd, coord), padx=0, pady=0)
+      temp_buttonmap_dict[coord].grid(row=y, column=x)
+      temp_buttonmap_dict[coord].bind('<Button-3>', partial(self.right_minecmd, coord))
+    self.buttonmap_dict = temp_buttonmap_dict
 
   def lay_label(self, coord):
     x = coord[0]
     y = coord[1]
-    if self.minemap[y][x]%10 == 0:
-      self.buttonmap[y][x] = Label(self.root, image=self.photo_dict['0'])
-      self.buttonmap[y][x].grid(row=y, column=x)
-    elif self.minemap[y][x]%10 == 9:
-      self.buttonmap[y][x] = Label(self.root, image=self.photo_dict['mine'])
-      self.buttonmap[y][x].grid(row=y, column=x)
+    if self.minemap[coord]%10 == 0:
+      self.buttonmap_dict[coord] = Label(self.root, image=self.photo_dict['0'], padx=0, pady=0)
+      self.buttonmap_dict[coord].grid(row=y, column=x)
+    elif self.minemap[coord]%10 == 9:
+      self.buttonmap_dict[coord] = Label(self.root, image=self.photo_dict['mine'], padx=0, pady=0)
+      self.buttonmap_dict[coord].grid(row=y, column=x)
     else:
-      self.buttonmap[y][x] = Label(self.root, image=self.photo_dict[f'{self.minemap[y][x]%10}'])
-      self.buttonmap[y][x].bind('<Button-1>', partial(self.label_leftclick, coord))
-      self.buttonmap[y][x].bind('<Button-3>', partial(self.label_rightclick, coord))
-      self.buttonmap[y][x].grid(row=y, column=x)
+      self.buttonmap_dict[coord] = Label(self.root, image=self.photo_dict[f'{self.minemap[coord]%10}'], padx=0, pady=0, borderwidth=0)
+      self.buttonmap_dict[coord].bind('<Button-1>', partial(self.label_leftclick, coord))
+      self.buttonmap_dict[coord].bind('<Button-3>', partial(self.label_rightclick, coord))
+      self.buttonmap_dict[coord].grid(row=y, column=x)
 
   def openblock(self, coord):
-    rightend = len(self.minemap[0])
-    bottomend = len(self.minemap)
+    col = self.level_dict['col']
+    row = self.level_dict['row']
     # print(coord)
-    x = coord[0]
-    y = coord[1]
     coordset = {coord}
     majorset = {coord}
     minorset = {coord}
     checkorgo = 'go'
-    if self.minemap[y][x] % 10 == 0:
+    if self.minemap[coord] % 10 == 0:
       checkorgo = 'check'
 
     while checkorgo == 'check':
@@ -131,22 +123,22 @@ class mineland:
       for coordinate in majorset:
         i = coordinate[0]
         j = coordinate[1]
-        if self.minemap[j][i] % 10 == 0:
-          for m in range(max(0, i-1), min(i+2, rightend)):
-            for n in range(max(0, j-1), min(j+2, bottomend)):
+        if self.minemap[coordinate] % 10 == 0:
+          for m in range(max(0, i-1), min(i+2, col)):
+            for n in range(max(0, j-1), min(j+2, row)):
               minorset.add((m, n))
           minorset = minorset - coordset
       coordset = minorset | coordset
       checkorgo = 'go'
-      for (x, y) in minorset:
-        if self.minemap[y][x] % 10 == 0:
+      for coordinate in minorset:
+        if self.minemap[coordinate] % 10 == 0:
           checkorgo = 'check'
     
-    for (x, y) in coordset:
+    for coordinate in coordset:
       # print((x, y))
-      self.buttonmap[y][x].destroy()
-      self.lay_label((x, y))
-      self.openedset.add((x, y))
+      self.buttonmap_dict[coordinate].destroy()
+      self.lay_label(coordinate)
+      self.openedset.add(coordinate)
     # 만약 모든 지뢰가 아닌 칸을 열었다면
     if len(self.openedset) == self.level_dict['row']*self.level_dict['col'] - self.level_dict['mines']:
       self.allopened = True
@@ -162,7 +154,7 @@ class mineland:
   def dead(self):
     print('dead')
     for minecoord in self.mineset:
-        self.buttonmap[minecoord[1]][minecoord[0]].destroy()
+        self.buttonmap_dict[minecoord].destroy()
         self.lay_label(minecoord)
     self.restart_btn['image'] = self.photo_dict['dead']
 
@@ -172,7 +164,7 @@ class mineland:
 
   # 라벨 양쪽클릭 이벤트를 위한 함수
   def label_leftclick(self, coord, event):
-    print('labelleft')
+    # print('labelleft')
     self.lefton = True
     self.lefttime = time.time()
     if self.righton:
@@ -183,7 +175,7 @@ class mineland:
       pass
 
   def label_rightclick(self, coord, event):
-    print('labelright')
+    # print('labelright')
     self.righton = True
     self.righttime = time.time()
     if self.lefton:
@@ -194,63 +186,60 @@ class mineland:
       pass
 
   def label_bothclick(self, coord):
-    rightend = len(self.minemap[0])
-    bottomend = len(self.minemap)
+    col = self.level_dict['col']
+    row = self.level_dict['row']
     x = coord[0]
     y = coord[1]
-    print('bothclicked!')
+    # print('bothclicked!')
     self.righttime = self.lefttime = False
     temp_set = set()
-    for i in range(max(0, x-1), min(x+2, rightend)):
-      for j in range(max(0, y-1), min(y+2, bottomend)):
+    for i in range(max(0, x-1), min(x+2, col)):
+      for j in range(max(0, y-1), min(y+2, row)):
         temp_set.add((i, j))
     temp_set.remove(coord)
     temp_set -= self.openedset
     for coordinate in set(temp_set):
-      if self.buttonmap[coordinate[1]][coordinate[0]]['state'] == 'disabled':
+      if self.buttonmap_dict[coordinate]['state'] == 'disabled':
         temp_set.remove(coordinate)
-      elif self.minemap[coordinate[1]][coordinate[0]] % 10 == 9:
+      elif self.minemap[coordinate] % 10 == 9:
         self.dead()
-        pass
     
     while temp_set:
       self.openblock(temp_set.pop())
       temp_set -= self.openedset
 
-# 함수 정의
-def lay_mine(minemap, mines):
-  rownum = len(minemap)
-  colnum = len(minemap[0])
-  mineset = set()
-  temp_set = [i for i in range(rownum*colnum)]
-  shuffle(temp_set)
-  for mine in range(mines):
-    minexy = temp_set.pop()
-    # print(minexy)
-    minemap[minexy//colnum][minexy%colnum] = 9
-  for y, row in enumerate(minemap):
-    for x, block in enumerate(row):
-      if block != 9:
-        minemap[y][x] = assign_label_num(minemap, x, y)
+  def lay_mine(self):
+    temp_minemap = {(i, j): 0 for i in range(self.level_dict['col']) for j in range(self.level_dict['row'])}
+    temp_mineset = set()
+    shuffle(coordinates:=list(temp_minemap.keys()))
+    temp_mineset = set(coordinates[:self.level_dict['mines']])
+    self.minemap = dict(temp_minemap)
+    self.mineset = set()
+    for minecoord in temp_mineset:
+      self.minemap[minecoord] = 9
+    for coord in temp_minemap.keys():
+      if self.minemap[coord] != 9:
+        self.minemap[coord] = self.assign_label_num(coord[0], coord[1])
       else:
-        mineset.add((x, y))
-  return minemap, mineset
+        self.mineset.add(coord)
 
-def assign_label_num(minemap, x, y):
-  '''
-  지뢰가 없는 칸에 대하여 그 칸의 라벨 넘버를 
-  '''
-  rightend = len(minemap[0])
-  bottomend = len(minemap)
-  temp_list = []
-  mines = 0
-  for i in range(max(x-1, 0), min(rightend, x+2)):
-    for j in range(max(y-1, 0), min(bottomend, y+2)):
-      temp_list.append((i, j))
-  for xx, yy in temp_list:
-    if minemap[yy][xx] % 10 == 9:
-      mines += 1
-  return mines
+  def assign_label_num(self, x, y):
+    '''
+    지뢰가 없는 칸에 대하여 그 칸의 라벨 넘버를 
+    '''
+    col = self.level_dict['col']
+    row = self.level_dict['row']
+    temp_set = set()
+    mines = 0
+    for i in range(max(x-1, 0), min(col, x+2)):
+      for j in range(max(y-1, 0), min(row, y+2)):
+        temp_set.add((i, j))
+    for temp_coord in temp_set:
+      if self.minemap[temp_coord] % 10 == 9:
+        mines += 1
+    return mines
+
+# 함수 정의
 
 # 상수 정의
 level_dict_dict = {
